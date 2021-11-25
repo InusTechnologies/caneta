3c4404b0963068f128cabc435a7c1ca897886b3f
92257852915 <artur.santos@serpro.gov.br>
2021-11-25 10:57:23 -0300

    classes

---
+++ src/carro/Carro.java
@@ -0,0 +1,11 @@
+package carro;
+
+public class Carro {
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        String marca = "sw4";
+        int ano = 2015;
+        String modelo = "pe3";
+
+    }
+}
---
+++ src/carro/Suv.java
@@ -0,0 +1,10 @@
+package carro;
+
+public class Suv {
+    String marca;
+    int ano;
+    String modelo;
+    void status(){
+        System.out.print("Uma carro " +this.marca);
+    }
+}
